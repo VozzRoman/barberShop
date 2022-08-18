503d04f0a8b6b16bfbf9f4fae3088b6e7f3c6243
VozzRoman <looper0500@ukr.net>
2022-08-18 17:10:57 +0300

    addin

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 
+const BurgerButton = document.querySelector(".button-burger");
+const BurgerMenu = document.querySelector(".header__menu");
+
+BurgerButton.addEventListener('click', function() {
+  BurgerMenu.classList.toggle('active');
+});
+
 const swiper = new Swiper('.section-slider__swiper', {
   // Optional parameters
+  	autoplay: {
+		delay: 2000,
+	  },
+  direction: 'horizontal',
   loop: true,
-
   // If we need pagination
    pagination: {
 		el: '.swiper-pagination',
 		paginationClickable: true,
-    	type: 'bullets',
+    type: 'bullets',
   },
 
   // Navigation arrows
