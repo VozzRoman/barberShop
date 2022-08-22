c4a4d3dfe5dc2f82380e6f3768fa93d2953683be
VozzRoman <looper0500@ukr.net>
2022-08-22 19:40:21 +0300

    add

--- js/script.js
+++ js/script.js
@@ -12,7 +12,6 @@ const swiper = new Swiper('.section-slider__swiper', {
 		delay: 2000,
 	  },
   direction: 'horizontal',
-  loop: true,
   // If we need pagination
    pagination: {
 		el: '.swiper-pagination',
@@ -27,3 +26,18 @@ const swiper = new Swiper('.section-slider__swiper', {
   },
 
 });
+//-------прокрутка------//
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
