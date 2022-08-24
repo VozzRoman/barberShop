4f6d83682dbc05ba8c7c18da7e95b920647439e7
VozzRoman <looper0500@ukr.net>
2022-08-24 16:12:42 +0300

    add animation

--- js/script.js
+++ js/script.js
@@ -41,3 +41,46 @@ for (let anchor of anchors) {
     })
   })
 }
+
+//---------Анимация------//
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll (params) {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart =4;
+			
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+			if(animItemHeight > window.innerHeight){
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+			if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('_active');
+
+
+			}else{
+				if (!animItem.classList.contains('_anim-no-hide')) { //Класс _anim-no-hide убирает повторную анимацию при скроле
+				animItem.classList.remove('_active');
+
+				}
+			}
+			
+		}
+	}
+	function offset(el) {  //Функуия получения координат элемента относительно документа
+		var rect = el.getBoundingClientRect(),
+		scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+		scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+	}
+setTimeout(() => { //Время задержки анимации
+	animOnScroll();
+}, 300);
+
+	
+}
